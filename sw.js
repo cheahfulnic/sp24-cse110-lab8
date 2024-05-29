008e74062134425f64e7a2db277fd1e5f9fa5324
nicholas cheah <nicholasseancheah@gmail.com>
2024-05-29 04:26:03 -0700

    test completed 6

--- sw.js
+++ sw.js
@@ -9,15 +9,15 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
       //            added to the cache when the ServiceWorker is installed
-      const RECIPE_URLs = [
+      const RECIPE_URLS = [
         'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
         'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
         'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
         'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
         'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
         'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
-      ];
-      return cache.addAll(RECIPE_URLs);
+    ];
+      return cache.addAll(RECIPE_URLS);
     })
   );
 });
@@ -45,14 +45,12 @@ self.addEventListener('fetch', async function (event) {
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
-  const cache = await caches.open(CACHE_NAME);
-  const cachedResponse = await cache.match(event.request);
-  if (cachedResponse) {
-    return cachedResponse;
-  } else {
-    const networkResponse = await fetch(event.request);
-    const responseClone = networkResponse.clone();
-    await cache.put(event.request, responseClone);
-    return networkResponse;
-  }
+  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+    return cache.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
+        cache.put(event.request, fetchedResponse.clone());
+        return fetchedResponse;
+      });
+    });
+  }));
 });
