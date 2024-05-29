c1119d5dfba7ec577b752bf4af8e96d517747fdf
nicholas cheah <nicholasseancheah@gmail.com>
2024-05-29 00:12:55 -0700

    test completed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -57,7 +57,7 @@ function initializeServiceWorker() {
   if ("serviceWorker" in navigator) {
     window.addEventListener('load', async () => {
       try {
-        const registration = await navigator.serviceWorker.register("/sw.js", {
+        const registration = await navigator.serviceWorker.register("/sp24-cse110-lab8//sw.js", {
           scope: "/",
         });
         if (registration.installing) {
